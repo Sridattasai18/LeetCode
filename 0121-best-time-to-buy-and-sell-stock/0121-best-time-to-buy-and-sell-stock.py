fb8dd5bde8693e4c6d9d32fb75697a01a05eb134
Kaligotla Sri Datta Sai Vithal <kaligotlasridattasai18@gmail.com>
2026-08-25 22:03:51 +0530

    Time: 65 ms (22.14%), Space: 28.7 MB (41.6%) - Code Sync

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
+        minval = prices[0]
         profit = 0
-        for i in prices:
-            min_price = min(i,min_price)
-            current_profit = i - min_price
-            profit = max(current_profit,profit)
-        
+        for i in range(1,len(prices)):
+            profit = max(profit, prices[i]-minval)
+            minval = min(minval, prices[i])
         return profit
+        
