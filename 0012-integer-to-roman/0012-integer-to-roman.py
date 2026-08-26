fee762f95f287013dceda056b23f32ea429d7657
Kaligotla Sri Datta Sai Vithal <kaligotlasridattasai18@gmail.com>
2026-08-26 22:42:36 +0530

    Time: 6 ms (42.67%), Space: 19.3 MB (26.06%) - Code Sync

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        value_symbols = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+            (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
+
+        res = []
+
+        for values, symbols in value_symbols:
+            if num == 0:
+                break
+            count = num // values
+            res.append(count * symbols)
+            num -= count * values
+
+        return ''.join(res)        
